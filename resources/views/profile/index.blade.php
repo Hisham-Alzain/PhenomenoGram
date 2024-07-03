96abc6519c379ff06dd11835a9325f3659b51d84
ThePhenomenal-Z <alzaintab@gmail.com>
2024-07-03 09:56:33 +0300

    .

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-5 pt-7 ">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMLtHFpEfPc1E--Yn3fad12QFJuFbxpY_SRA&usqp=CAU" height="190" width="190"class="rounded-circle" >
+            <img src="https://i0.wp.com/getstamped.co.uk/wp-content/uploads/WebsiteAssets/Placeholder.jpg" height="190" width="190"class="rounded-circle" >
         </div>
         <div class="col-7 pt-6">
             <div class="d-flex justify-content-between align-items-baseline" >
@@ -12,9 +12,9 @@
                 <a href="#">Add New post</a>
         </div>
             <div class="d-flex" >
-                <div class="pr-9"><strong> 3</strong> posts </div>
-                <div class="pr-9"><strong> 670 </strong> followers </div>
-                <div class="pr-9"><strong>  270  </strong> following </div>
+                <div class="pr-9"><strong> 0</strong> posts </div>
+                <div class="pr-9"><strong> 0 </strong> followers </div>
+                <div class="pr-9"><strong>  0  </strong> following </div>
             </div>
             <div class="pt-4" ><strong>{{$user ->profile->title ?? 'fill in the blank'}} </strong></div>
             <div >{{$user ->profile->description ?? 'fill in the blank' }}</div>
@@ -25,6 +25,7 @@
         <div class="col-4">
             <img src="/storage/{{$post->image }}" class="w- 100">
         </div>
+        @endforeach
     </div>
 </div>
 @endsection
